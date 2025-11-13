c3e12547935f20d4a6a15a993b95b616c2fabe22
Min-Duc1705 <146452065+Min-Duc1705@users.noreply.github.com>
2025-11-13 20:18:29 +0700

    commit

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,6 +1,9 @@
 import { WebhookEvent } from '@clerk/nextjs/server';
 import { EventType } from './../../../../node_modules/svix/src/api/eventType';
 import { Webhook } from "svix";
+import { create } from 'domain';
+import createUser from '@/lib/actions/user.actions';
+import { NextResponse } from 'next/server';
 
 
 export async function POST(req: Request) {
@@ -10,6 +13,9 @@ export async function POST(req: Request) {
 
   if(!process.env.WEBHOOK_SECRET) {
     throw new Error("WEBHOOK_SECRET is not defined");
+  }
+  if(!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Bad Request", { status: 400 });
   }
   const payload = await req.json();
   const body = JSON.stringify(payload);
@@ -29,10 +35,20 @@ export async function POST(req: Request) {
   }
 const EventType = msg.type;
  if(EventType === "user.created") {
-    msg.data
+  const {id,username,email_addresses,image_url} = msg.data;
+  const user = await createUser({
+    clerkId: id,
+    username: username!,
+    name: username!,
+    email: email_addresses[0].email_address,
+    avatar: image_url,
+  })
+    return NextResponse.json({ message: "OK", user });
  }
 
   // Rest
+
+
 
   return new Response("OK", { status: 200 });
 }
